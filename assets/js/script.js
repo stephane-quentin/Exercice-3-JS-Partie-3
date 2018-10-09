87fba031601a3aa1450c010eb9e60e748afb6b78
Stiouf <stephane.quentin@live.fr>
2018-10-09 14:50:14 +0200

    ex3

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function green(){
+  document.getElementById('text').style.color = 'green';
+}
+function red(){
+  document.getElementById('text').style.color = 'red';
+}
+function blue(){
+  document.getElementById('text').style.color = 'blue';
+}
