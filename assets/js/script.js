bb1df316095728955d53b4dbfdd8f894b6c9ee34
Stiouf <stephane.quentin@live.fr>
2018-10-11 10:07:44 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
-function green(){
-  document.getElementById('text').style.color = 'green';
-}
-function red(){
-  document.getElementById('text').style.color = 'red';
-}
-function blue(){
-  document.getElementById('text').style.color = 'blue';
+// function green(){
+//   document.getElementById('text').style.color = 'green';
+// }
+// function red(){
+//   document.getElementById('text').style.color = 'red';
+// }
+// function blue(){
+//   document.getElementById('text').style.color = 'blue';
+// }
+
+
+function changeColor(newColor){
+  document.getElementById('text').style.color = newColor;
 }
